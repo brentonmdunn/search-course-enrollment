7d41c59e2a9b239f90e2df4d0033966f69b90c45
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-08-05 10:12:26 -0700

    Add looping until user quit

--- main.py
+++ main.py
@@ -26,43 +26,49 @@ def main():
     with open("enrollment_window.json", 'r', encoding='utf8') as file:
         enrollment_window = json.load(file)
 
-    for quarter in enrollment_window:
-        print("> " + quarter)
-    quarter = input_f.input_quarter(enrollment_window)
-    all_courses_path = "data/" + quarter + "/all_courses.txt"
+    while True:
 
-    with open(all_courses_path, 'r', encoding='utf8') as file:
-        all_courses = file.read()
-    all_courses_list = all_courses.split('\n')
-
-    selected_class = input_f.input_class(all_courses_list)
-
-    file_name = "data/" + quarter + "/raw/" + selected_class + ".csv"
-    csv_file = csv.reader(open(file_name, 'r', encoding='utf8'), delimiter=',')
-
-    first_row = True
-    found_data = False
-    for row in csv_file:
-        if first_row:
-            first_row = False
-            continue
-        if time_f.get_date_standard(row[0]) not in enrollment_window[quarter]:
-            continue
-        if int(row[2]) == 0:
-
-            found_data = True
-
-            hour = time_f.get_hour(row[0]) % 12
-            am_pm = "AM"
-            if time_f.get_hour(row[0]) > 12:
-                am_pm = "PM"
-            minute = time_f.get_minute(row[0])
-            time = f"{hour}:0{minute} {am_pm}" if minute < 10 else f"{hour}:{minute} {am_pm}"
-            print(format_output(quarter, selected_class, time,
-                enrollment_window[quarter][time_f.get_date_standard(row[0])]))
+        for quarter in enrollment_window:
+            print("> " + quarter)
+        quarter = input_f.input_quarter(enrollment_window)
+        if quarter is None:
             break
-    if not found_data:
-        print(f"{selected_class} always had seats available.")
+        all_courses_path = "data/" + quarter + "/all_courses.txt"
+
+        with open(all_courses_path, 'r', encoding='utf8') as file:
+            all_courses = file.read()
+        all_courses_list = all_courses.split('\n')
+
+        selected_class = input_f.input_class(all_courses_list)
+        if selected_class is None:
+            break
+
+        file_name = "data/" + quarter + "/raw/" + selected_class + ".csv"
+        csv_file = csv.reader(open(file_name, 'r', encoding='utf8'), delimiter=',')
+
+        first_row = True
+        found_data = False
+        for row in csv_file:
+            if first_row:
+                first_row = False
+                continue
+            if time_f.get_date_standard(row[0]) not in enrollment_window[quarter]:
+                continue
+            if int(row[2]) == 0:
+
+                found_data = True
+
+                hour = time_f.get_hour(row[0]) % 12
+                am_pm = "AM"
+                if time_f.get_hour(row[0]) > 12:
+                    am_pm = "PM"
+                minute = time_f.get_minute(row[0])
+                time = f"{hour}:0{minute} {am_pm}" if minute < 10 else f"{hour}:{minute} {am_pm}"
+                print(format_output(quarter, selected_class, time,
+                    enrollment_window[quarter][time_f.get_date_standard(row[0])]))
+                break
+        if not found_data:
+            print(f"{selected_class} always had seats available.")
 
 if __name__ == "__main__":
     main()
